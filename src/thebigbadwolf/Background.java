5bdea300de325847bb06a2d209f1b3190e2281c0
jj139811 <jj139811@kaist.ac.kr>
2022-10-20 16:04:52 +0900

    refactored background and place

--- src/thebigbadwolf/Background.java
+++ src/thebigbadwolf/Background.java
@@ -1,12 +1,19 @@
 package thebigbadwolf;
 
+import java.awt.Image;
+import java.io.IOException;
+
 public class Background extends Place {
     //field
     BBWTheater theater = BBWTheater.getInstance();
+    private Image mBackgroundImage = null;
     
     //constructor
     protected Background(String name) {
         super(name);
+        
+        String fileName = "bg/bg_" + name;
+        this.mBackgroundImage = this.loadImage(fileName);
     }
     
     //method
--- src/thebigbadwolf/Place.java
+++ src/thebigbadwolf/Place.java
@@ -2,10 +2,13 @@
 package thebigbadwolf;
 
 import java.awt.Image;
+import java.awt.Point;
 import java.util.ArrayList;
 
 public class Place extends PlayObject {
-    
+    //constants
+    private static final int DISTANCE_BETWEEN_MEMBERS = 20;
+    private static final int DISTANCE_BELOW_PLACE = 30;
     //field
     ArrayList<PlayObject> mMembers = new ArrayList<PlayObject>();
     public ArrayList<PlayObject> getmembers() {
@@ -20,10 +23,22 @@ public class Place extends PlayObject {
     //method
     public void add(PlayObject po) {
         this.mMembers.add(po);
+        this.rearrangeMembers();
+    }
+    
+    private void rearrangeMembers() {
+        for (int i = 0; i < this.mMembers.size(); i++) {
+            PlayObject po = this.mMembers.get(i);
+            po.setPosition(new Point(
+                this.getPosition().x +
+                (this.mMembers.size() / 2 + i) * DISTANCE_BETWEEN_MEMBERS,
+                this.getPosition().y + DISTANCE_BELOW_PLACE));
+        }
     }
     
     public void remove(PlayObject po) {
         this.mMembers.remove(po);
+        this.rearrangeMembers();
     }
 
     @Override
